11c74e13458cd088a28a5e2912b57e4f789a1f66
Anna B <aniabartkowiak19@op.pl>
2021-04-16 18:32:19 +0200

    Fixed a font error.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import "typeface-open-sans";
-import "typeface-merriweather";
+import "typeface-rubik";
 
 ReactDOM.render(
   <React.StrictMode>
